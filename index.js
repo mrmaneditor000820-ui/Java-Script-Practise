1fae269c6e4a216b6eda39b090447adf38365b56
dengerbhai00820-com <dengerbhai00820@gmail.com>
2025-12-14 07:11:10 -0800

    add new file

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+
+//var decre = a-- + a--
+
+//conso//
+
+//var prompt = require('prompt-sync')();
+
+//var number =+prompt("enter a number")
+//var sum = number + 1
+//console.log("sum=>", sum)
+
+// var i=0; // initialize 
+// i>0 ; // condition
+// i++; //updation
+// var number=Number(prompt("enter your number"));
+//for (var i = 1; i <11; i++){
+//console.log(2 +"  x   "+i + "  =  " +(i * 2));
+// }
+
+// var uname = prompt("enter your name")
+// var absentStudent = "ali"
+
+// if(uname === absentStudent){
+//     console.log("you are not allow")
+// }else{
+//     console.log("you are allow")
+// }
+   
+
+// for lops practise 
+
+// for(let i = 1; i < 100; i++){
+//     console.log(i)
+// }
+
+// for(let i = 100; i > 0; i--){
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 1; i <= 19; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// let fruits = ["apple", "banana", "cherry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+
+// let fruits = ["apple", "banana", "cherry"];
+
+// for (let i = fruits.length -1; i >= 0; i--) {
+//   console.log(fruits[i]);
+// }
+
