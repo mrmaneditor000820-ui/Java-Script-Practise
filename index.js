04817d8708db52074d161dbf4ba61d66e3cc9e50
dengerbhai00820-com <dengerbhai00820@gmail.com>
2025-12-14 20:42:11 -0800

    updated

--- index.js
+++ index.js
@@ -63,4 +63,37 @@
 // for (let i = fruits.length -1; i >= 0; i--) {
 //   console.log(fruits[i]);
 // }
+// Nested array/for loop
+
+// var twoDArray = [
+//   [1,2,3,],
+//   [4,5,6],
+//   [7,8,9,[2,"g",["c",10]]]
+// ];
+
+// console.log("twoDimensionArray =>", twoDArray)
+// console.log("lenght", twoDArray.length)
+// console.log(twoDArray[2][3][2][0])
+
+// var arr1 =[[1,2,3], [4,5,6],[7,8,9]]
+// var arr2 =[[9,8,7], [6,5,4],[3,2,1]]
+
+// var sumOfArr =arr1[0][0] + arr2[0][0];
+// console.log("sum", sumOfArr)
+
+// var twoDArr= [[1,2,3],[4,5,6],[7,8,9]]
+// for(i = 0; i<twoDArr.length; i++ ) {
+//   console.log(twoDArr[i][0])
+//   for(j = 0; j<twoDArr[i].length; j++ ) {
+//   console.log(twoDArr[i][j])
+//   }
+// }
+
+// / var i=0; // initialize 
+// i>0 ; // condition
+// i++; //updation
+// var number=Number(prompt("enter your number"));
+// for (var i = 1; i < 11; i++){
+// console.log(number +"  x   "+i + "  =  " +(i * number));
+// }
 
