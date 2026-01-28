ab4d12cd1b00b2ee07ddb8220782585068dd76c8
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-01-28 19:44:23 +0500

    started

--- script.js
+++ script.js
@@ -159,7 +159,7 @@
 // }
 
 var checkstudt = prompt("enter student name")
-var checkstudtwithcapitallett = checkstudt.toLowerCase()
+var firstchar = 
 var studt = ["Ali","Ahmed","Asad","Bilal","Rehman"];
 for(var i = 0; i < studt.length; i++){
     if(studt[i]== checkstudtwithcapitallett ){
